26ea0c928ccc933dcc878d6a9c5b48b1c423aeda
miyeon <myksphone2001@gmail.com>
2026-02-09 16:39:45 +0900

    local에서 수정함

--- src/kosta/serviece/GoodsService.java
+++ src/kosta/serviece/GoodsService.java
@@ -8,4 +8,7 @@ public class GoodsService {
 		System.out.println("select 추가 ...");
 		System.out.println("시나리오 3번 test");
 	}
+	public void update() {
+		System.out.println("local에서 update 수정");
+	}
 }

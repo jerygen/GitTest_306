92dc5b1c8e381d5ad6b28651b6daa42411e5c03e
miyeon <myksphone2001@gmail.com>
2026-02-10 14:53:02 +0900

    시작 전

--- src/kosta/controller/MemberController.java
+++ src/kosta/controller/MemberController.java
@@ -4,4 +4,7 @@ public class MemberController {
 	public void insert() {
 		System.out.println("로컬 member 브랜치에서 member 클래스 추가");
 	}
+	public void select() {
+		System.out.println("select 메소드 추가");
+	}
 }
